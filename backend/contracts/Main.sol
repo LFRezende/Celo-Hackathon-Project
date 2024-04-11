2507d9e9e59927008a83e497426c771eb82ae73c
LFRezende <lfsrsprofessional@gmail.com>
2024-04-11 16:37:17 -0300

    Update: Delegate function in main and Modifiers in Proxy

--- backend/contracts/Main.sol
+++ backend/contracts/Main.sol
@@ -5,10 +5,34 @@ import "./Proxy.sol";
 contract Main {
     // Boss delegates to a subordinate an amount (to be transfered to new contract)
 
+    // Mappings Main
+    mapping(address => mapping(uint256 => address)) public ownerToContract;
+    mapping(address => uint256) public amountOfContracts;
+
+    // Events
+
+    event delegationConfirmed(
+        address indexed owner,
+        address payable[] indexed delegated,
+        address payable[] indexed receivers
+    );
+
     function delegate(
         address payable[] calldata _delegate,
-        address payable[] calldata _allowedRx, uint256[] calldata _amountRx
+        address payable[] calldata _allowedRx,
+        uint256[] calldata _amountRx
     ) public payable returns (bool success) {
         Proxy proxy_contract = new Proxy(_delegate, _allowedRx, _amountRx); // New contract
+        address payable proxy_address = payable(address(proxy_contract));
+
+        ownerToContract[msg.sender][
+            amountOfContracts[msg.sender]
+        ] = proxy_address;
+        amountOfContracts[msg.sender] += 1;
+
+        (success, ) = proxy_address.call{value: msg.value}("");
+        emit delegationConfirmed(msg.sender, _delegate, _allowedRx);
+
+        return success;
     }
 }
--- backend/contracts/Proxy.sol
+++ backend/contracts/Proxy.sol
@@ -1,4 +1,5 @@
 // SPDX0License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract Proxy {
@@ -8,7 +9,26 @@ contract Proxy {
     address payable[] public allowed_rx;
 
     // Mappings
-    mapping(address => uint256) public permitedAmount;
+    mapping(address => uint256) public permittedAmount;
+    mapping(address => bool) public rxAllowed;
+
+    // Modifiers:
+
+    modifier AllowedRx(address payable _wallet) {
+        require(
+            rxAllowed[_wallet],
+            "Receiver is not allowed - talk to delegator. "
+        );
+        _;
+    }
+
+    modifier AmountPermitted(uint256 _amount, address payable _wallet) {
+        require(
+            _amount <= permittedAmount[_wallet],
+            "Surpasses allowed bugdet"
+        );
+        _;
+    }
 
     // Constructor
     constructor(
@@ -20,11 +40,20 @@ contract Proxy {
         delegated = _delegated;
         allowed_rx = _allowed_rx;
         for (uint256 i = 0; i < _amountRx.length; i++) {
-            permitedAmount[_allowed_rx[i]] = _amountRx[i];
+            permittedAmount[_allowed_rx[i]] = _amountRx[i];
+            rxAllowed[_allowed_rx[i]] = true;
         }
     }
 
     // Functions
 
-    function transferToPermitted() public payable returns (bool success) {}
+    function transferToPermitted(
+        address payable  _rx
+    )
+        public
+        payable
+        AllowedRx(_rx)
+        AmountPermitted(msg.value, _rx)
+        returns (bool success)
+    {}
 }
