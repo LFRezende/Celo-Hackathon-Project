f87c54c33c98a5877f63fa69b92c77ff8ee22018
LFRezende <lfsrsprofessional@gmail.com>
2024-04-11 18:12:20 -0300

    Update: Change the funding to outside function out of delegation

--- backend/contracts/Main.sol
+++ backend/contracts/Main.sol
@@ -8,14 +8,32 @@ contract Main {
     // Mappings Main
     mapping(address => mapping(uint256 => address)) public ownerToContract;
     mapping(address => uint256) public amountOfContracts;
+    mapping(address => bool) public isOwnerOfContract; // Gets the owner of the contract
+    mapping(address => uint256) public amountFundedByOwner;
 
     // Events
-
     event delegationConfirmed(
         address indexed owner,
         address payable[] indexed delegated,
         address payable[] indexed receivers
     );
+
+    // Modifiers:
+    modifier onlyOwner(address payable _rxContract) {
+        require(
+            isOwnerOfContract[_rxContract],
+            "Only owner can call this function"
+        );
+        _;
+    }
+
+    modifier hasDepositedEnough(uint256 _fundAmount) {
+        require(
+            amountFundedByOwner[msg.sender] >= _fundAmount,
+            "Not enough funds for this. Deposit some more."
+        );
+        _;
+    }
 
     function delegate(
         address payable[] calldata _delegate,
@@ -30,9 +48,28 @@ contract Main {
         ] = proxy_address;
         amountOfContracts[msg.sender] += 1;
 
-        (success, ) = proxy_address.call{value: msg.value}("");
+        //(success, ) = proxy_address.call{value: msg.value}("");
         emit delegationConfirmed(msg.sender, _delegate, _allowedRx);
 
         return success;
+    }
+
+    function fundContract(
+        address payable _rxContract,
+        uint256 _fundAmount
+    )
+        public
+        payable
+        onlyOwner(_rxContract)
+        hasDepositedEnough(_fundAmount)
+        returns (bool success)
+    {
+        (success, ) = _rxContract.call{value: _fundAmount}("");
+        require(success, "Transaction Failed");
+        return success;
+    }
+
+    function deposit() public payable {
+        amountFundedByOwner[msg.sender] += msg.value;
     }
 }
